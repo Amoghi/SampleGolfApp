cc9c9e77d4ca453edc845a91f1f1f7bff8da4eea
Amogh Indurkar <amoghindurkar@gmail.com>
2019-09-25 12:17:03 +0530

    Initial Commit

---
+++ SampleGolf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleGolf
+//
+//  Created by Amogh Indurkar on 25/09/19.
+//  Copyright © 2019 Amogh Indurkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
